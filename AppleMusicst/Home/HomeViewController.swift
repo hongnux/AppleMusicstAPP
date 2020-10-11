4535f6d3268d84b48bb1b4ff08173d5d84f4c405
hongnux <hom2955@gmail.com>
2020-10-11 19:08:58 +0900

    HeaderView 구성

--- AppleMusicst/Home/HomeViewController.swift
+++ AppleMusicst/Home/HomeViewController.swift
@@ -39,7 +39,20 @@ extension HomeViewController: UICollectionViewDataSource {
         switch kind {
         case UICollectionView.elementKindSectionHeader:
             // TODO: 헤더 구성하기
-            return UICollectionReusableView()
+            guard let item = trackManager.todaysTrack else {
+                return UICollectionReusableView()
+            }
+            guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "TrackCollectionHeaderView", for: indexPath) as? TrackCollectionHeaderView else {
+                return UICollectionReusableView()
+            }
+            
+            header.update(with: item)
+            header.tapHandler = { item -> Void in
+                // 클릭시 해당곡의 Player를 띄워 재생
+                print("---> item : \(item.convertToTrack()?.title)")
+            }
+            return header
+            
         default:
             return UICollectionReusableView()
         }
